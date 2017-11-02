91b86dbff29f5f82e4ccffdf967818f82d84bbc8
Evan <evan.st.burton@gmail.com>
2017-11-01 18:54:38 -0700

    Fixed indentation, added more comments

--- pivot.m
+++ pivot.m
@@ -4,23 +4,26 @@ m = size(col, 1);
 
 %BEGIN_PIVOT = [CARRY, col]
 
+% This is to account for CARRY and col having their obj. coefficients in
+% the first row.
 piv_row = piv_row+1;
 
 % Scale pivot row
 CARRY(piv_row, :) = CARRY(piv_row, :)/col(piv_row);
+% Set pivot value to 1 in case of round-off error
 col(piv_row) = 1;
 
-% Apply elimination only to CARRY
+% Apply elimination
 for i = 1:m
     if i ~= piv_row
         factor = -col(i);
         CARRY(i, :) = CARRY(i, :) + factor*CARRY(piv_row, :);
         col(i) = 0;
-       % pause
+        % pause
     end
 end
 
-%PIVOT_RESULT = [CARRY, col]
+%PIVOT_RESULT = CARRY
 
 end
 
--- simplex.m
+++ simplex.m
@@ -1,5 +1,5 @@
 function [ x_opt, z_opt ] = simplex(z0, c, A, b)
-%Solves the linear program given by 
+%Solves the linear program given by
 %
 %        minimize z = c*x + z0
 %        subject to Ax=b.
@@ -32,21 +32,21 @@ rows = zeros(1, rows_to_keep);
 if rows_to_remove > 0
     k = 1;
     for i = 1:m
-
+        
         if basis(i) < n && k <= rows_to_keep
-
+            
             rows(k) = i;
-
+            
             k = k+1;
         end
-
+        
     end
-
+    
     %rows
     A_star = A_star(rows, :);
     basis = basis(rows);
     CARRY = CARRY([1, 1+rows], 1:m);
-
+    
     m = size(A_star, 1);
 end
 
